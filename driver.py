b55f8dbfd4327d054383b4d13cbdcf43d4084e1f
Sam Liebman <sammyliebman@gmail.com>
2018-07-03 12:55:29 -0400

    Done.

--- driver.py
+++ driver.py
@@ -1,12 +1,19 @@
 # remeber to import the trip class here
-
+from trip import Trip
 
 class Driver:
     def __init__(self, name):
         self.name = name
 
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        self._name = name
+
     def my_trips(self):
-        pass
+        return Trip.my_trips(self)
 
     def my_trip_summaries(self):
-        pass
+        return Trip.my_trip_summaries(self)
