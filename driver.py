3e2b7d5f6ac713114d1bce0f914a37f21ba8cec4
Ari Horwitz <arihorwitz@Yoninas-MacBook-Air.local>
2018-07-06 09:36:08 -0400

    Done.

--- driver.py
+++ driver.py
@@ -1,12 +1,28 @@
 # remeber to import the trip class here
-
+from trip import Trip
 
 class Driver:
     def __init__(self, name):
         self.name = name
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
     def my_trips(self):
-        pass
+        # trippies = []
+        # for going in Trip.all():
+        #     if going.driver == self:
+        #         trippies.append(going)
+        # return trippies
+        return [going for going in Trip.all() if going.driver == self]
 
     def my_trip_summaries(self):
-        pass
+        names_o_trips = []
+        for place in self.my_trips():
+            names_o_trips.append(str(place.start) + " to " + str(place.destination))
+        return names_o_trips
