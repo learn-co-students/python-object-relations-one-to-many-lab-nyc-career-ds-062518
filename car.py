135990e47840f65db7f8b1c8db842032dde369bf
Christopher Pease <chris.m.pease@gmail.com>
2018-07-06 09:25:48 -0400

    Done.

--- car.py
+++ car.py
@@ -1,8 +1,30 @@
 class Car:
+
     _all = []
 
-    def __init__(self, make, model, year):
+    def __init__(self, make, model, year, owner):
         self._make = make
         self._model = model
         self._year = year
-        #remember to associate a car with its owner
+        self._owner = owner
+        Car._all.append(self)
+
+    @property
+    def make(self):
+        return self._make
+
+    @property
+    def model(self):
+        return self._model
+
+    @property
+    def year(self):
+        return self._year
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @classmethod
+    def all(cls):
+        return cls._all
--- driver.py
+++ driver.py
@@ -1,12 +1,15 @@
-# remeber to import the trip class here
-
+from trip import Trip
 
 class Driver:
     def __init__(self, name):
-        self.name = name
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
 
     def my_trips(self):
-        pass
+        return [trip for trip in Trip.all() if trip._driver.name == self.name]
 
     def my_trip_summaries(self):
-        pass
+        return [trip._start + ' to ' + trip._destination for trip in self.my_trips()]
