17c0ab96cc06049a98d5aa9ee92d5f4e6323ce13
Catherine Huang <cath.h858@gmail.com>
2018-07-07 15:38:21 -0400

    Done.

--- driver.py
+++ driver.py
@@ -1,12 +1,15 @@
-# remeber to import the trip class here
-
+from trip import Trip# remeber to import the trip class here
 
 class Driver:
     def __init__(self, name):
-        self.name = name
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
 
     def my_trips(self):
-        pass
+        return [trip for trip in Trip.all() if trip.driver == self]
 
     def my_trip_summaries(self):
-        pass
+        return ['{} to {}'.format(trip.start, trip.destination) for trip in self.my_trips()]
--- owner.py
+++ owner.py
@@ -1,10 +1,17 @@
-# import car class here
-
+from car import Car# import car class here
 
 class Owner:
     def __init__(self, name, age):
         self._name = name
         self._age = age
 
+    @property
+    def name (self):
+        return self._name
+    @property
+    def age(self):
+        return self._age
+    def my_cars(self):
+        return [car for car in Car.all() if car.owner==self ]
     def find_my_cars(self):
-        pass
+        return ['{} {}'.format(car.make, car.model) for car in self.my_cars()]
