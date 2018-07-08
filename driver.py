747aa52872ca15f8ecf0a4d77843bcf3ffd09a92
naokosuga <ns2586@columbia.edu>
2018-07-08 19:24:50 -0400

    Done.

--- driver.py
+++ driver.py
@@ -1,12 +1,22 @@
 # remeber to import the trip class here
-
+from trip import Trip
 
 class Driver:
     def __init__(self, name):
-        self.name = name
+        self._name = name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self,name):
+        self._name = name
 
     def my_trips(self):
+        return [trip for trip in Trip.all() if trip.driver == self]
         pass
 
     def my_trip_summaries(self):
+        return [trip.start + " to " + trip.destination for trip in Trip.all() if trip.driver == self]
         pass
